e7fb3a8e3471a25fd8f96cbd2d8429ffda1a4612
Valentin Peltier <valentinpeltier@yahoo.fr>
2020-11-15 00:19:13 +0100

    feat: add Card and Stack classes (#1)
    * feat: add Card class
    
    * feat: add Stack class

--- src/fr/utt/lo02/tdvp/core/Card.java
+++ src/fr/utt/lo02/tdvp/core/Card.java
@@ -1,40 +1,97 @@
 package fr.utt.lo02.tdvp.core;
 
-public class Card {
-    private String shape;
+class Card {
+    /**
+     * The possible shapes of a card
+     */
+    public enum Shape {
+        SQUARE,
+        TRIANGLE,
+        CIRCLE,
+    }
 
-    private String color;
+    /**
+     * The possible colors of a card
+     */
+    public enum Color {
+        RED,
+        GREEN,
+        BLUE,
+    }
 
+    private Shape shape;
+    private Color color;
     private boolean filled;
 
-    public String getShape() {
-        // Automatically generated method. Please delete this comment before entering specific code.
+    /**
+     * Instantiate a card
+     * @param shape the shape of the card
+     * @param color the color of the card
+     * @param filled the filling of the card
+     */
+    public Card(Shape shape, Color color, boolean filled) {
+        this.shape = shape;
+        this.color = color;
+        this.filled = filled;
+    }
+
+    /**
+     * Get the shape of the card
+     * @return the shape of the card
+     */
+    public Shape getShape() {
         return this.shape;
     }
 
-    public void setShape(String value) {
-        // Automatically generated method. Please delete this comment before entering specific code.
-        this.shape = value;
-    }
-
-    public String getColor() {
-        // Automatically generated method. Please delete this comment before entering specific code.
+    /**
+     * Get the color of the card
+     * @return the color of the card
+     */
+    public Color getColor() {
         return this.color;
     }
 
-    public void setColor(String value) {
-        // Automatically generated method. Please delete this comment before entering specific code.
-        this.color = value;
-    }
-
-    public boolean isFilled() {
-        // Automatically generated method. Please delete this comment before entering specific code.
+    /**
+     * Get the filling of the card
+     * @return the filling of the card
+     */
+    public boolean getFilled() {
         return this.filled;
     }
 
-    public void setFilled(boolean value) {
-        // Automatically generated method. Please delete this comment before entering specific code.
-        this.filled = value;
-    }
+    /**
+     * Parse the card to a string defining the card
+     * @return a string defining the card
+     */
+    public String toString() {
+        String shape, color, filled;
 
+        // Shape
+        switch (this.shape) {
+            case SQUARE: shape = "carré";
+                break;
+            case TRIANGLE: shape = "triangle";
+                break;
+            case CIRCLE: shape = "cercle";
+                break;
+            default: shape = this.shape.toString();
+        }
+
+        // Color
+        switch (this.color) {
+            case RED: color = "rouge";
+                break;
+            case GREEN: color = "vert";
+                break;
+            case BLUE: color = "bleu";
+                break;
+            default: color = this.color.toString();
+        }
+
+        // Filled
+        if (this.filled) filled = "rempli";
+        else filled = "non rempli";
+
+        return shape + ", " + color + ", " + filled;
+    }
 }
--- src/fr/utt/lo02/tdvp/core/Stack.java
+++ src/fr/utt/lo02/tdvp/core/Stack.java
@@ -1,18 +1,49 @@
 package fr.utt.lo02.tdvp.core;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.LinkedList;
+import java.util.NoSuchElementException;
+import java.util.Collections;
 
-public class Stack {
-    private List<Card> cards = new ArrayList<Card> ();
+class Stack {
+    private static Stack instance = new Stack();
 
-    public Card drawCard() {
-    }
+    private LinkedList<Card> cards = new LinkedList<Card>();
 
     private Stack() {
+        // Initialize the stack
+        this.reset();
     }
 
+    /**
+     * Get the single instance of the class
+     * @return the single instance of Stack
+     */
     public static Stack getInstance() {
+        return instance;
     }
 
+    /**
+     * Draw a card from the stack. Throw an exception if the stack is empty
+     * @return the drawn card
+     * @throws NoSuchElementException
+     */
+    public Card drawCard() throws NoSuchElementException {
+        return cards.pop();
+    }
+
+    /**
+     * Fill the stack with all cards and shuffle it
+     */
+    public void reset() {
+        // Add every possible card to the stack
+        for (Card.Shape shape: Card.Shape.values()) {
+            for (Card.Color color: Card.Color.values()) {
+                cards.add(new Card(shape, color, false));
+                cards.add(new Card(shape, color, true));
+            }
+        }
+
+        // Shuffle
+        Collections.shuffle(cards);
+    }
 }
