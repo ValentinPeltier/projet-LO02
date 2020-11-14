1be71c778f7c86f5be5f4e8a1570c66d82beddcd
ValentinPeltier <valentinpeltier@yahoo.fr>
2020-11-14 19:21:27 +0100

    fix: minor fixes

---
+++ src/fr/utt/lo02/tdvp/Main.java
@@ -0,0 +1,9 @@
+package fr.utt.lo02.tdvp;
+
+import fr.utt.lo02.tdvp.core.GameManager;
+
+public class Main {
+	public static void main(String[] args) {
+		GameManager.getInstance().initializeGame();
+	}
+}
