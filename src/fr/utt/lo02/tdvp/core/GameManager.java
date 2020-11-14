e9e467c18afa858e86d57c53cf3dfe008b71561a
ValentinPeltier <valentinpeltier@yahoo.fr>
2020-11-14 19:21:07 +0100

    feat: implement GameManager singleton

--- src/fr/utt/lo02/tdvp/core/GameManager.java
+++ src/fr/utt/lo02/tdvp/core/GameManager.java
@@ -4,18 +4,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GameManager {
-    private List<PlayerContext> players = new ArrayList<PlayerContext> ();
+    private static GameManager instance = new GameManager();
 
-    private void initializeGame() {
-    }
+    private List<PlayerContext> players = new ArrayList<PlayerContext>();
 
-    private void playGame() {
-    }
+    private GameManager() {}
 
-    private GameManager() {
-    }
-
+    /**
+     * Get the single instance of the class
+     * @return the single instance of GameManager
+     */
     public static GameManager getInstance() {
+        return instance;
     }
 
+    public void initializeGame() {
+        // TODO
+    }
+
+    public void playGame() {
+        // TODO
+    }
 }
