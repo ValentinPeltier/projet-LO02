59a1f04add1908651b68960c00eb144b32d0240d
ValentinPeltier <valentinpeltier@yahoo.fr>
2020-11-24 22:43:20 +0100

    feat: add Input.promptChoice method

--- src/fr/utt/lo02/tdvp/core/cli/Input.java
+++ src/fr/utt/lo02/tdvp/core/cli/Input.java
@@ -9,9 +9,68 @@ public class Input {
 
     /**
      * Returns the unique scanner of the application
-     * @return the instance of the scanner
+     * @return The instance of the scanner
      */
     public static Scanner getScanner() {
         return scanner;
+    }
+
+    /**
+     * Asks the user to choose from the possible answers. The index of the first anwser will be 1.
+     * @param title The title displayed before the possible answers
+     * @param answers The possible answers to display
+     * @param question The question displayed before asking for the answer
+     * @return The index of the answer choosen
+     */
+    public static int promptChoice(String title, String[] answers, String question) {
+        return promptChoice(title, answers, question, 1);
+    }
+
+    /**
+     * Asks the user to choose from the possible answers
+     * @param title The title displayed before the possible answers
+     * @param answers The possible answers to display
+     * @param question The question displayed before asking for the answer
+     * @param firstIndex The index of the first answer
+     * @return The index of the answer choosen
+     */
+    public static int promptChoice(String title, String[] answers, String question, int firstIndex) {
+        int choice = 0;
+        boolean isValid;
+
+        do {
+            // Display the title
+            System.out.println("=== " + title + " ===\n");
+
+            // Display answers one by one
+            for (int i = 0; i < answers.length; i++) {
+                final int displayIndex = i + firstIndex;
+                System.out.println(displayIndex + ") " + answers[i]);
+            }
+
+            // Ask the user to make a choice
+            System.out.print("\n> " + question + " ");
+
+            try {
+                choice = getScanner().nextInt();
+
+                // Check if the choice is valid
+                isValid = choice >= firstIndex && choice < answers.length + firstIndex;
+            }
+            catch(Exception e) {
+                // Certainly a InputMismatchException exception. Anyway, the choice cannot be valid.
+                isValid = false;
+
+                // Clear the scanner buffer
+                getScanner().nextLine();
+            }
+
+            // Inform the user that the choice is invalid
+            if(!isValid) {
+                System.out.println("Choix invalide.\n");
+            }
+        } while(!isValid); // Ask until the answer is valid
+
+        return choice;
     }
 }
