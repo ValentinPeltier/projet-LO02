ef24d12960607b545b635ac62cb28b1b6af7293d
ValentinPeltier <valentinpeltier@yahoo.fr>
2020-11-24 22:44:53 +0100

    feat: add variant related classes

---
+++ src/fr/utt/lo02/tdvp/core/variant/VariantSecondChance.java
@@ -0,0 +1,21 @@
+package fr.utt.lo02.tdvp.core.variant;
+
+public class VariantSecondChance extends Variant {
+    static final String name = "Seconde chance";
+    static final String description = "donne la possibilité à tous les joueurs de re-piocher une VictoryCard en milieu de partie";
+
+    public static String getName() {
+        return name;
+    }
+
+    public static String getDescription() {
+        return description;
+    }
+
+    /**
+     * Executes the variant
+     */
+    public void execute() {
+        // TODO
+    }
+}
