9bfbde64385088368ad22a183657a1b8ed5d5c63
ValentinPeltier <valentinpeltier@yahoo.fr>
2020-11-25 09:29:55 +0100

    feat: initialize variant

--- src/fr/utt/lo02/tdvp/core/GameManager.java
+++ src/fr/utt/lo02/tdvp/core/GameManager.java
@@ -3,11 +3,18 @@ package fr.utt.lo02.tdvp.core;
 import java.util.ArrayList;
 import java.util.List;
 
+import fr.utt.lo02.tdvp.core.cli.Input;
 import fr.utt.lo02.tdvp.core.player.Player;
+import fr.utt.lo02.tdvp.core.variant.Variant;
+import fr.utt.lo02.tdvp.core.variant.VariantRandomSwitch;
+import fr.utt.lo02.tdvp.core.variant.VariantSecondChance;
+
 public class GameManager {
     private static GameManager instance = new GameManager();
 
     private List<Player> players = new ArrayList<Player>();
+
+    private Variant variant;
 
     private GameManager() {}
 
@@ -19,8 +26,42 @@ public class GameManager {
         return instance;
     }
 
+    /**
+     * Initialize a new game.
+     * Choose a variant, a number of players, their names and how many virtual players there should be.
+     */
     public void initializeGame() {
-        // TODO
+        // Initialize the variant
+        initializeVariant();
+    }
+
+    /**
+     * Asks the user to choose a variant
+     */
+    private void initializeVariant() {
+        // Ask the user for a variant
+        int variantChoice = Input.promptChoice("Choix de la variante", new String[]{
+            "Aucune",
+            VariantRandomSwitch.getName() + " (" + VariantRandomSwitch.getDescription() + ")",
+            VariantSecondChance.getName() + " (" + VariantSecondChance.getDescription() + ")"
+        }, "Quelle variante voulez-vous choisir ?", 0);
+
+        // Set the variant
+        switch(variantChoice) {
+            case 0:
+                // Create a variant that does nothing
+                this.variant = new Variant() {
+                    public void execute() {}
+                };
+                break;
+            case 1:
+                // RandomSwitch variant
+                this.variant = new VariantRandomSwitch();
+                break;
+            case 2:
+                this.variant = new VariantSecondChance();
+                break;
+        }
     }
 
     public void playGame() {
