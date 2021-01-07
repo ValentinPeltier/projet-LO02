c0e27cc68e2147319ce661ab4b1cd11e576b852a
ValentinPeltier <valentinpeltier@yahoo.fr>
2021-01-08 00:50:53 +0100

    fix: Window

--- src/fr/utt/lo02/tdvp/view/gui/Window.java
+++ src/fr/utt/lo02/tdvp/view/gui/Window.java
@@ -5,28 +5,61 @@ import javafx.application.Platform;
 import javafx.event.EventHandler;
 import javafx.scene.*;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
+import fr.utt.lo02.tdvp.model.panel.*;
+import fr.utt.lo02.tdvp.view.gui.utils.ImageUtil;
 
 public class Window extends Application {
-    private static Scene scene;
+    private static StackPane root;
 
     public static void setPanel(Pane panel) {
-        scene.setRoot(panel);
+        if (root.getChildren().size() > 1) {
+            root.getChildren().remove(1);
+        }
+        root.getChildren().add(panel);
     }
 
     public static void initWindow() {
         launch();
     }
 
+    public static void displayPopup(String content) {
+        displayPopup("", content);
+    }
+
+    public static void displayPopup(String title, String content) {
+        PopupPanelView.getInstance().setText(title, content);
+        root.getChildren().get(0).setVisible(true);
+    }
+
+    public static void hidePopup() {
+        root.getChildren().get(0).setVisible(false);
+    }
+
     @Override
     public void start(Stage window) {
-        scene = new Scene(HomePanel.getInstance(), 800, 400);
+        // Initialize panels
+        HomePanel.getInstance().init();
+        SettingsPanel.getInstance().init();
+        GamePanel.getInstance().init();
+        PopupPanel.getInstance().init();
+
+        // Display the pop up panel above everything
+        root = new StackPane(PopupPanelView.getInstance());
+        root.getChildren().get(0).setViewOrder(-1);
+        root.getChildren().get(0).setVisible(false);
+
+        setPanel(HomePanelView.getInstance());
+
+        Scene scene = new Scene(root, 800, 400);
         scene.getStylesheets().add("/styles/global.css");
 
         window.setTitle("Shape Up !");
-        window.getIcons().add(Images.getImage("icon.png"));
+        window.getIcons().add(ImageUtil.getImage("icon.png"));
         window.setMaximized(true);
+        window.setResizable(false);
         window.setScene(scene);
         window.setOnCloseRequest(new EventHandler<WindowEvent>(){
             @Override
